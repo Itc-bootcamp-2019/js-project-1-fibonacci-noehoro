67c653ec7d6994b8f3b974dc8d38c36a127d9163
noehoro <noehoro@gmail.com>
2020-03-01 16:04:59 +0200

    milestone 3

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-let text = document.getElementById('text');
-let x = 3;
-let y = 2;
-text.innerText = `The Fibonacci of ${x} is ${y}`;
+let text = document.getElementById("text");
+
+let set = [];
+let y = 10; //Until where do you wanna go?
+
+function calc(index) {
+    set[0] = 0;
+    set[1] = 1;
+  for (let i = 0; i < index; i++) {
+    set[i+2] = set[i]+set[i+1];
+    console.log(`The Fib of ${i+2}is  ${set[i+2]}`);
+  }
+}
+
+calc(y);
+
+text.innerText = `The Fibonacci of ${y} is ${set[]}`;
