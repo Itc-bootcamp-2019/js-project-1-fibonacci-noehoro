784319a2c6c9f6c76b6fe9696a9b3bdecfca6613
noehoro <noehoro@gmail.com>
2020-03-03 11:40:14 +0200

    Milestone 4

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
-let text = document.getElementById("text");
+let text = document.getElementById("answer");
 
 let set = [];
 let y = 10; //Until where do you wanna go?
 
-function calc(index) {
+function calc(y) {
     set[0] = 0;
     set[1] = 1;
-  for (let i = 0; i < index; i++) {
+  for (let i = 0; i < y; i++) {
     set[i+2] = set[i]+set[i+1];
-    console.log(`The Fib of ${i+2}is  ${set[i+2]}`);
+    console.log(`The Fib of ${i+1}is  ${set[i+1]}`);
+  }
+  text.innerText = `${set[y]}`;
+}
+
+function validateForm() {
+  var x = document.getElementById("question").value;
+  if (x == "") {
+    alert("Number must be filled out");
+    return false;
   }
 }
 
-calc(y);
 
-text.innerText = `The Fibonacci of ${y} is ${set[y]}`;
+function buttonClicked() {
+  var x = document.getElementById("question").value;
+  if (x == "") {
+    alert("Number must be filled out!");
+    return false;
+  } else if(!(x>0) || !(x<20)){
+    alert("Must put in a number in between 0 and 20!");
+    return false;
+  }
+  calc(x);
+}
+
+
+
+
+
+
+
