f4d980a92f930a75e7871b397b07ac935d936cd7
noehoro <noehoro@gmail.com>
2020-03-03 14:35:58 +0200

    Milestone 5

--- main.js
+++ main.js
@@ -9,13 +9,26 @@ function fib(num) {
 }
 
 function buttonClicked() {
-  var x = document.getElementById("question").value;
-  if (x == "") {
-    alert("Number must be filled out!");
-    return false;
-  } else if (!(x > 0) || !(x < 20)) {
-    alert("Must put in a number in between 0 and 20!");
-    return false;
-  }
-  text.innerText = `${fib(x)}`;
+  let http = new XMLHttpRequest();
+  const url = "http://localhost:5050/fibonacci/" + document.getElementById("question").value;
+  http.open("GET", url, true);
+  http.onreadystatechange = function() {
+    if (http.readyState == 4 && http.status == 200) {
+      let f = http.responseText;
+      const a = JSON.parse(f).result;
+      text.innerText = a;
+    }
+  };
+  http.send();
 }
+
+
+// var x = document.getElementById("question").value;
+//   if (x == "") {
+//     alert("Number must be filled out!");
+//     return false;
+//   } else if (!(x > 0) || !(x < 20)) {
+//     alert("Must put in a number in between 0 and 20!");
+//     return false;
+//   }
+//   text.innerText = `${fib(x)}`;
