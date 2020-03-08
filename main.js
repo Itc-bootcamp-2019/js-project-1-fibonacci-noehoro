ac803b6a701aa13b9f8e9ab45de44e8f4725eb04
noehoro <noehoro@gmail.com>
2020-03-08 13:10:28 +0200

    Milestone 6

--- main.js
+++ main.js
@@ -1,34 +1,74 @@
-let text = document.getElementById("answer");
+function show(tag) {
+  document.getElementById(tag).classList.remove("hide");
+}
 
-function fib(num) {
-  if (num === 0 || num === 1) {
-    return num;
-  } else {
-    return fib(num - 2) + fib(num - 1);
+function hide(tag) {
+  document.getElementById(tag).classList.add("hide");
+}
+
+function error(message) {
+  show("alert-row");
+  hide("spinner");
+  document.getElementById("alert-message").innerHTML = message;
+  return false;
+}
+
+function validation(x) {
+  switch (true) {
+    case x === "":
+      error("Must type something!");
+      return false;
+      break;
+    case x < 1:
+      error("Must be bigger than 0!");
+      return false;
+      break;
+    case x > 50:
+      error("Must be smaller than 50!");
+      return false;
+      break;
+    case isNaN(x):
+      error("Must be a number!");
+      return false;
+      break;
+    case x % 1 !== 0:
+      error("Cannot be a decimal!");
+      return false;
+      break;
+    default:
+      return true;
   }
 }
 
 function buttonClicked() {
-  let http = new XMLHttpRequest();
-  const url = "http://localhost:5050/fibonacci/" + document.getElementById("question").value;
-  http.open("GET", url, true);
-  http.onreadystatechange = function() {
-    if (http.readyState == 4 && http.status == 200) {
-      let f = http.responseText;
-      const a = JSON.parse(f).result;
-      text.innerText = a;
-    }
-  };
-  http.send();
+  let answer = document.getElementById("answer");
+  let error = document.getElementById("error");
+  hide("answer");
+  hide("error");
+  hide("alert-row");
+  show("spinner");
+  let index = document.getElementById("input").value;
+  if (validation(index)) {
+    fetch("http://localhost:5050/fibonacci/" + index)
+      .then(response => {
+        if (response.status === 400) {
+          return response.text();
+        } else if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then(data => {
+        if (typeof data === "object") {
+          answer.innerHTML = data.result;
+          hide("spinner");
+          show("answer");
+        } else {
+          hide("spinner");
+          show("error");
+          error.innerText = data;
+        }
+      });
+  }
 }
 
-
-// var x = document.getElementById("question").value;
-//   if (x == "") {
-//     alert("Number must be filled out!");
-//     return false;
-//   } else if (!(x > 0) || !(x < 20)) {
-//     alert("Must put in a number in between 0 and 20!");
-//     return false;
-//   }
-//   text.innerText = `${fib(x)}`;
+document.getElementById("button").addEventListener("click", buttonClicked);
